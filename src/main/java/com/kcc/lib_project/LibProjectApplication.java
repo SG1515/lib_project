234e5284829873a95456b76a1385019efcc4b919
woo0doo <duden5000@gmail.com>
2024-08-29 11:05:56 +0900

    init

---
+++ src/main/java/com/kcc/lib_project/LibProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kcc.lib_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcc/lib_project/LibProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.lib_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
