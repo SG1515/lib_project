19a9a2a65bd6951eb90bd5d2a2248d0da05b29f9
Seong_gu <sunggu507@naver.com>
2024-09-01 21:03:58 +0900

    feat: security dependency 추가 및 모든 요청에 권한주기 및 csrf검사 disable

---
+++ src/main/java/com/kcc/lib_project/global/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.kcc.lib_project.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+  private static final String[] WHITE_LIST = {
+    "/common/**",
+    "WEB-INF/views/**",
+    ""
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .authorizeHttpRequests(authorizeHttpRequests ->
+        authorizeHttpRequests.anyRequest().permitAll()
+        )
+        .csrf(csrf -> csrf.disable());
+
+      return http.build();
+  }
+
+}
